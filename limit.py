4fc029a776f2f7ac9480eb02d6c1607653822726
amirsam1389 <127240299+amirsam1389@users.noreply.github.com>
2023-12-19 21:23:25 +0330

    Update limit.py

--- limit.py
+++ limit.py
@@ -3,6 +3,11 @@
 import argparse
 import subprocess
 
+# ANSI escape code for yellow and blue colors
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+RESET = '\033[0m'
+
 
 def limit_bandwidth(interface, upload_limit, download_limit):
     subprocess.run(['tc', 'qdisc', 'del', 'dev', interface, 'root'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
@@ -11,15 +16,18 @@ def limit_bandwidth(interface, upload_limit, download_limit):
     subprocess.run(['tc', 'qdisc', 'add', 'dev', interface, 'parent', '1:10', 'handle', '10:', 'sfq', 'perturb', '10'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     subprocess.run(['tc', 'class', 'add', 'dev', interface, 'parent', '1:', 'classid', '1:11', 'htb', 'rate', upload_limit], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     subprocess.run(['tc', 'qdisc', 'add', 'dev', interface, 'parent', '1:11', 'handle', '11:', 'sfq', 'perturb', '10'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    print(f'پهنای باند دانلود محدود به {download_limit} و پهنای باند آپلود محدود به {upload_limit} شد.')
+    print(f'{YELLOW}Please enter the network interface name (example: eth0): {RESET}')
+    print(f'{YELLOW}Please enter the desired upload bandwidth (example: 250Mbit): {RESET}')
+    print(f'{YELLOW}Please enter the desired download bandwidth (example: 250Mbit): {RESET}')
+    print(f'{BLUE}The download bandwidth is limited to {download_limit} and the upload bandwidth is limited to {upload_limit}.{RESET}')
 
 
 def main():
-    parser = argparse.ArgumentParser(description='محدود کردن پهنای باند دانلود و آپلود کاربران TCP')
+    parser = argparse.ArgumentParser(description='Limit download and upload bandwidth of TCP users')
 
-    interface = input("لطفاً نام رابط شبکه را وارد کنید (مثلاً eth0): ")
-    upload_limit = input("لطفاً پهنای باند آپلود مورد نظر را وارد کنید (مثلاً 1Mbit): ")
-    download_limit = input("لطفاً پهنای باند دانلود مورد نظر را وارد کنید (مثلاً 2Mbit): ")
+    interface = input(f'{YELLOW}Please enter the network interface name (example: eth0): {RESET}')
+    upload_limit = input(f'{YELLOW}Please enter the desired upload bandwidth (example: 250Mbit): {RESET}')
+    download_limit = input(f'{YELLOW}Please enter the desired download bandwidth (example: 250Mbit): {RESET}')
 
     limit_bandwidth(interface, upload_limit, download_limit)
 
